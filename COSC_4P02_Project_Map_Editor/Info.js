e64b665261cd84d83751d2ed32305330fce5dcee
ko19af <80547887+ko19af@users.noreply.github.com>
2023-03-02 21:41:40 -0500

    Add files via upload

---
+++ COSC_4P02_Project_Map_Editor/Info.js
@@ -0,0 +1,27 @@
+const list = JSON.parse(sessionStorage.getItem("group"));// get list info from session storage
+
+function submitInfo(form) {// collect information from form and store to associated tiles (THIS NEEDS WORK !!!!!)
+	var value = document.getElementsByName('wing');// create array holding radio buttons
+	var wing;// create variable to hold value of selected radio button
+	
+	//for(var radio of value){// go though all of the radio buttons
+		//if(radio.checked) {// if button was checked
+			//wing = radio.value;// collect value
+			//break;// exit search
+		//}
+	//}
+	//if (wing == null) alert("please select a wing");
+	//else alert(form.eName.value  + " " + form.endDate.value + " " + form.floor.value + " " + wing);
+};
+
+function myFunction(){
+		let current = list.head;
+		while(current){
+			console.log(current.data.tile);
+			current = current.next;
+		}
+	}
+	
+function loadInfo(){
+	
+};
