329387219349e9b0f7494697827c1f65a7b21dfb
ko19af <80547887+ko19af@users.noreply.github.com>
2023-03-14 09:46:42 -0400

    Update Start.js

--- COSC_4P02_Project_Map_Editor/Start.js
+++ COSC_4P02_Project_Map_Editor/Start.js
@@ -1,5 +1,5 @@
 function begin(form) {
-	sessionStorage.setItem("rFloors", JSON.stringify(form.floors));// set what floor we are at in the input stage
+	sessionStorage.setItem("rFloors", JSON.stringify(form.floors.value));// set what floor we are at in the input stage
 	sessionStorage.setItem("cMap", JSON.stringify(mapping = new Map()));// initialize hash map to hold map data
 	sessionStorage.setItem("mInfo", JSON.stringify({mName: form.mName.value, numFloors: form.floors.value,}));
 	window.open('tile.html');// open next page
