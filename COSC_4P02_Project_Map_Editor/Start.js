901e95cc8fd963e3cd599baf4309e2be15de55d9
ko19af <80547887+ko19af@users.noreply.github.com>
2023-03-25 16:12:30 -0400

    Update Start.js

--- COSC_4P02_Project_Map_Editor/Start.js
+++ COSC_4P02_Project_Map_Editor/Start.js
@@ -1,7 +1,11 @@
 function begin(form) {
+
+	const museumMap = [];// create array to hold museum map info
+	museumMap[0] = {layout: [0], fInfo: [{eName:"pre-America", location:" ",eEd: " ", floorNum: " ", tile: 0},],};// initialize layout of array
+	
 	sessionStorage.setItem("rFloors", JSON.stringify(form.floors.value));// set what floor we are at in the input stage
-	sessionStorage.setItem("cMap", JSON.stringify(mapping = new Map()));// initialize hash map to hold map data
+	sessionStorage.setItem(form.mName.value, JSON.stringify(museumMap));// initialize hash map to hold map data
 	sessionStorage.setItem("mInfo", JSON.stringify({mName: form.mName.value, numFloors: form.floors.value,}));
-	window.open('tile.html');// open next page
+	window.open('floorPlan.html');// open next page
 	myWindow.close();
 };
