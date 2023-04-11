2ff177e13b29e313cf4c4ab9c42a9906ad79d25a
ko19af <80547887+ko19af@users.noreply.github.com>
2023-04-11 13:48:29 -0400

    Update dataBase.js

--- COSC_4P02_Project_Map_Editor/dataBase.js
+++ COSC_4P02_Project_Map_Editor/dataBase.js
@@ -13,7 +13,7 @@ const firebaseConfig = {
   firebase.initializeApp(firebaseConfig);
   
   // reference database
-  var contactFormDB = firebase.database().ref("Map");
+  var database = firebase.database();
   
   if(document.getElementById("finish")){
   	document.getElementById("finish").addEventListener("click", submitMap);
@@ -22,34 +22,56 @@ const firebaseConfig = {
   
   function submitMap(e) {
   	e.preventDefault();
-  	var name = JSON.parse(sessionStorage.getItem("mInfo")).mName;
-  	var map = JSON.parse(sessionStorage.getItem(name));
+  	var name = JSON.parse(sessionStorage.getItem("mInfo")).mName;// get map name from storage
+  	var map = JSON.parse(sessionStorage.getItem(name));// get map info from storage
   	
-  	saveMap(name, map);
-  	window.open("Search.html");
-  	window.close();
+  	if(JSON.parse(sessionStorage.getItem("update"))) {// if update is true in session storage
+  		database.ref("Map/" + name).update({
+  			map : map,});
+  		alert("map updated");
+  		sessionStorage.clear();// clear session storage
+  	}else {// if not updating map info
+  		database.ref("Map/" + name).set({
+  	      name : name,
+  	      map : map,
+  		});
+  		alert("Map saved");
+  	}
+  	window.open("Search.html");//open search (main) page
+  	window.close();// close window
+  }
+  
+  function deleteMap(e) {
+  	var name = JSON.parse(sessionStorage.getItem("mName"));
+  	database = firebase.database();
+  	
+  	database.ref("Map/" + name).remove();// reference data being removed and remove it
+  	
+  	alert("Musueum map " + name + " removed");// alert user museum was removed
+  	sessionStorage.clear();
+  	location.reload();// reload page so ot reflects change to database
   }
   
   function loadMaps(e){
   	sessionStorage.clear();
-  	contactFormDB = firebase.database().ref("Map/");
+  	database = firebase.database().ref("Map/");
    let list = document.getElementById("buttons")// get html element for holding buttons
   
-  	contactFormDB.on("child_added", function(data) {
+  	database.on("child_added", function(data) {
   		data.forEach(function(data){
   		if(data.key != "name"){
   			map = (data.val());
   		}else{
-  			var name = data.val();
-  			const newDiv = document.createElement("div");
-  			var p = document.createElement("p");
-  			var x = document.createElement("BUTTON");// create button object
-			var t = document.createTextNode("View: " + name);// attach button specefic text
-			x.appendChild(t);// attach text to button
-			x.addEventListener("click", function(){sessionStorage.setItem(name, JSON.stringify(map));
-							        sessionStorage.setItem("mInfo", JSON.stringify({mName: name})); 
-								window.open("Viewer.html"); 
-								window.close();});// on click load map info
+  				var name = data.val();
+  				const newDiv = document.createElement("div");
+  				var p = document.createElement("p");
+  				var x = document.createElement("BUTTON");// create button object
+				var t = document.createTextNode("View: " + name);// attach button specefic text
+				x.appendChild(t);// attach text to button
+				x.addEventListener("click", function(){sessionStorage.setItem(name, JSON.stringify(map));
+																	sessionStorage.setItem("mInfo", JSON.stringify({mName: name})); 
+																	window.open("Viewer.html"); 
+																	window.close();});// on click load map info
 				p.appendChild(x);
   				
   				if (true) {// CHANGE TO CHECK FOR ADMIN LOGGED IN
@@ -78,14 +100,18 @@ const firebaseConfig = {
 					var ab2 = document.createElement("div");
 					ab2.classList.add("dropdown-content")
 					var b2 = document.createElement("BUTTON");
-					b2.addEventListener("click", function() { sessionStorage.setItem("mInfo", JSON.stringify({mName:name,})); 
-										sessionStorage.setItem(name, JSON.stringify(map)); 
-										window.open("floorPreview.html"); 
-										window.close();});
+					b2.addEventListener("click", function() {
+														sessionStorage.setItem("mInfo", JSON.stringify({mName:name,})); 
+														sessionStorage.setItem(name, JSON.stringify(map)); 
+														sessionStorage.setItem("update", JSON.stringify(true));
+														window.open("floorPreview.html"); 
+														window.close();});
 					b2.appendChild(document.createTextNode("Edit Map"));
 					ab2.appendChild(b2);
 					var b3 = document.createElement("BUTTON");
-					b3.addEventListener("click", function() {deleteMuse(name);});
+					b3.addEventListener("click", function() {
+													   sessionStorage.setItem("mName", JSON.stringify(name));
+													   deleteMap();});
 					b3.appendChild(document.createTextNode("Delete"));
 					ab2.appendChild(b3);
 					admin2.appendChild(a2);
@@ -98,12 +124,4 @@ const firebaseConfig = {
   			}
   		});	
 	});
-  }
-  
-  const saveMap = (name, map) => {// NEED TO FIX DUPLICATION ISSUE!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-  	var newContactForm = contactFormDB.push();
-  	newContactForm.set({
-  	      name : name,
-  	      map : map,
-  	});
   }
