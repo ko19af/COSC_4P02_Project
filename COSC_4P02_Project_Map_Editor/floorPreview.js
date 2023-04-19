ddd334d95fd47a0759f3d5cbaa8580b0eb1711b0
ko19af <80547887+ko19af@users.noreply.github.com>
2023-04-19 15:58:56 -0400

    Update floorPreview.js

--- COSC_4P02_Project_Map_Editor/floorPreview.js
+++ COSC_4P02_Project_Map_Editor/floorPreview.js
@@ -39,12 +39,9 @@ sessionStorage.setItem("modify", JSON.stringify(1));// initialize modify floor t
   drawMap = function() {
 
     for (let index = 0; index < map.length; index ++) {
-
       buffer.fillStyle = (map[index] == 0)?"#FFFFFF":"#000000";
       buffer.fillRect((index % 16) * size, Math.floor(index/16) * size, size, size);
-
     }
-
   };
 
   loop = function(time_stamp) {
@@ -60,27 +57,18 @@ sessionStorage.setItem("modify", JSON.stringify(1));// initialize modify floor t
 
     context.drawImage(buffer.canvas, 0, 0, buffer.canvas.width, buffer.canvas.height, 0, 0, context.canvas.width, context.canvas.height);
 
-    output.innerHTML = "tile_x: " + tile_x + "<br>tile_y: " + tile_y + "<br>value: " + value;
-
     window.requestAnimationFrame(loop);
 
   };
 
   // just keeps the canvas element sized appropriately
   resize = function(event) {
-
     context.canvas.width = Math.floor(document.documentElement.clientWidth - 32);
-
     if (context.canvas.width > document.documentElement.clientHeight) {
-
       context.canvas.width = Math.floor(document.documentElement.clientHeight);
-
     }
-
     context.canvas.height = Math.floor(context.canvas.width * 0.5625);
-
     drawMap();
-
   };
   
   function displayInfo() {// this function displays the info in a tile on the column
@@ -94,8 +82,26 @@ sessionStorage.setItem("modify", JSON.stringify(1));// initialize modify floor t
   	
   	for(let x in info) {// load info from array into paragraph
   		if(x != "tile") {
-  			para = document.createElement("p");// create paragraph object
-  			para.innerText = info[x];// fill it with the info
+  			var para;// create paragraph object
+  			
+  			if(x == "eED") {
+  				para = document.createElement("p");
+  				para.innerText = "Exhibit End Date: \n" + info[x];// fill it with the info
+  			} else if (x == "eName"){
+  				para = document.createElement("p");
+  				para.innerText = "Exhibit Name: \n" + info[x];
+  			} else if (x == "floorNum"){
+  				para = document.createElement("p");
+  				para.innerText = "Floor: " + info[x];
+  			} else if (x == "eInfo") {
+  				para = document.createElement("div");
+  				para.innerText = "Exhibt Info: \n" + info[x];
+  				para.classList.add("ex3");
+  			} else {
+  				para = document.createElement("p");
+  				para.innerText = x + ": " + info[x];
+  			}
+  			
  			list.appendChild(para);// append it to info column
   		}		
   	}
@@ -112,18 +118,50 @@ sessionStorage.setItem("modify", JSON.stringify(1));// initialize modify floor t
 
 })();
 
-function buttonMaker(){
+function loadMuseData(){
 
 	let list = document.getElementById("buttons")// get html element for holding buttons
 	for (let value = 1; value <= (museumMap.length)-1; value++) {// needs to be altered to add buttons for all floors
 		const newDiv = document.createElement("div");
 		var x = document.createElement("BUTTON");// create button object
-		var t = document.createTextNode("View floor " + (value));// attach button specefic text
+		x.classList.add("buttonC");
+		var t = document.createTextNode("View Floor " + (value));// attach button specefic text
 		x.appendChild(t);// attach text to button
-		x.addEventListener("click", function() {map = museumMap[value].layout; sessionStorage.setItem("modify", JSON.stringify(value)); floor = value;});// set which floor is being modified});// attach function to load map on button click
+		x.addEventListener("click", function() {map = museumMap[value].layout; 
+															 sessionStorage.setItem("modify", JSON.stringify(value));
+															 document.getElementById("hereBeImages").innerHTML = "";
+															 floor = value; displayImages();});// set which floor is being modified});// attach function to load map on button click
 		newDiv.appendChild(x);
 		list.appendChild(newDiv);//append button to list of buttons
 	}
+	
+	displayImages();
+}
+
+function displayImages(){
+	let name = JSON.parse(sessionStorage.getItem("mInfo")).mName;
+  	let museumMap = JSON.parse(sessionStorage.getItem(name));
+  	let imgs = museumMap[floor].images;
+  			
+  	for(let x = 0; x < imgs.length; x++){
+  		var modal = document.getElementById("myModal");
+  		var modalImg = document.getElementById("img01");
+  		let newimg = document.createElement("img");
+  			
+  		newimg.src = imgs[x];
+  		newimg.setAttribute("id", "myImg2")
+  		newimg.style = "width:100%;max-width:150px";
+  		newimg.onclick = function() {
+  			modal.style.display = "block";
+  			modalImg.src = imgs[x];
+  		}
+  				
+  		var span = document.getElementsByClassName("close")[0];
+  		span.onclick = function(){
+  			modal.style.display = "none";
+  		}
+  		document.getElementById("hereBeImages").appendChild(newimg);
+  	}
 }
 
 function changeLayout() {
