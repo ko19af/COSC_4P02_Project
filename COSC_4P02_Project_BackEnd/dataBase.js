e54abcb089e63f75f28bae5ca46c7463cef9752b
Connor-Vrieswyk <70979573+Connor-Vrieswyk@users.noreply.github.com>
2023-04-23 18:21:17 -0400

    Fixed issue opening in new tab

--- COSC_4P02_Project_BackEnd/dataBase.js
+++ COSC_4P02_Project_BackEnd/dataBase.js
@@ -161,8 +161,10 @@ function begin(form) {// This function check if a map already exists on the fire
 		sessionStorage.setItem("rFloors", JSON.stringify(form.floors.value));// set what floor we are at in the input stage
 		sessionStorage.setItem(form.mName.value, JSON.stringify(museumMap));// initialize hash map to hold map data
 		sessionStorage.setItem("mInfo", JSON.stringify({mName: form.mName.value, numFloors: form.floors.value,}));
-		window.open('floorPlan.html');
-		window.close();
+		window.location = 'floorPlan.html';
+		return false;
+		//window.open('floorPlan.html');
+		//window.close();
 	} else {
 		alert("Museum already Exist");
 		sessionStorage.clear();
