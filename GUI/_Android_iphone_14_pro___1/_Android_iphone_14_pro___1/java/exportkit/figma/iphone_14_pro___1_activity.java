c5b871a7d37426c7edd1634ef3f0a1a48c2057c7
ss18yh <122497736+ss18yh@users.noreply.github.com>
2023-04-27 12:05:21 -0400

    Add files via upload

---
+++ GUI/_Android_iphone_14_pro___1/_Android_iphone_14_pro___1/java/exportkit/figma/iphone_14_pro___1_activity.java
@@ -0,0 +1,74 @@
+
+	 
+	/*
+	 *	This content is generated from the API File Info.
+	 *	(Alt+Shift+Ctrl+I).
+	 *
+	 *	@desc 		
+	 *	@file 		iphone_14_pro___1
+	 *	@date 		Tuesday 14th of March 2023 01:11:20 AM
+	 *	@title 		Page 1
+	 *	@author 	
+	 *	@keywords 	
+	 *	@generator 	Export Kit v1.3.figma
+	 *
+	 */
+	
+
+package exportkit.figma;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class iphone_14_pro___1_activity extends Activity {
+
+	
+	private View _bg__iphone_14_pro___1;
+	private ImageView contact_;
+	private ImageView background_pic__map_;
+	private ImageView search_bar;
+	private ImageView search_icon;
+	private ImageView top_right_nav;
+	private ImageView top_left_logo;
+	private View rectangle_4;
+	private TextView niagara_falls_history_museum;
+	private ImageView image_1;
+	private TextView places_and_addresses_5810_ferry_st__niagara_falls__on_l2g_1s9;
+	private ImageView image_2;
+	private View rectangle_3;
+	private View rectangle_5;
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.iphone_14_pro___1);
+
+		
+		_bg__iphone_14_pro___1 = (View) findViewById(R.id._bg__iphone_14_pro___1);
+		contact_ = (ImageView) findViewById(R.id.contact_);
+		background_pic__map_ = (ImageView) findViewById(R.id.background_pic__map_);
+		search_bar = (ImageView) findViewById(R.id.search_bar);
+		search_icon = (ImageView) findViewById(R.id.search_icon);
+		top_right_nav = (ImageView) findViewById(R.id.top_right_nav);
+		top_left_logo = (ImageView) findViewById(R.id.top_left_logo);
+		rectangle_4 = (View) findViewById(R.id.rectangle_4);
+		niagara_falls_history_museum = (TextView) findViewById(R.id.niagara_falls_history_museum);
+		image_1 = (ImageView) findViewById(R.id.image_1);
+		places_and_addresses_5810_ferry_st__niagara_falls__on_l2g_1s9 = (TextView) findViewById(R.id.places_and_addresses_5810_ferry_st__niagara_falls__on_l2g_1s9);
+		image_2 = (ImageView) findViewById(R.id.image_2);
+		rectangle_3 = (View) findViewById(R.id.rectangle_3);
+		rectangle_5 = (View) findViewById(R.id.rectangle_5);
+	
+		
+		//custom code goes here
+	
+	}
+}
+	
+	
